6d3f9b0cf07404df79ca9663a2db0884c016e6a7
Chee <ck_white@outlook.com>
2024-01-28 14:11:34 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution in this file!
+let companyName = 'Scuber';
+function companyName9(Scuber) {
+    /const companyName/
+}
+let mostProfitableNeighborhood = 'Chelsea';
+
+function mostProfitableNeighborhood(Chelesea) {
+    /let mostProfitableNeighborhood/
+}
+let companyCeo = 'Susan Smith';
+
+function companyCeo(Susan) {
+    /let companyCeo/
+}
